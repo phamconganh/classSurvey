120916bc50150e1c155a90772060053b18b05227
HoangPhu98 <hoangvanphu98@gmail.com>
2018-12-27 15:13:24 +0700

    font of survey

--- CLIENT-SIDE/src/app/manage-survey/manage-survey.component.ts
+++ CLIENT-SIDE/src/app/manage-survey/manage-survey.component.ts
@@ -1,21 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Ng2SmartTableModule, LocalDataSource } from 'ng2-smart-table';
+import { ModalDirective } from 'ngx-bootstrap/modal';
+import { TypeAction } from '../app.entity';
 
 @Component({
   selector: 'app-manage-survey',
   templateUrl: './manage-survey.component.html',
   styleUrls: ['./manage-survey.component.css']
 })
+
+
+
 export class ManageSurveyComponent implements OnInit {
 
-  constructor() { }
+  data: object = {
+    nameSubject: 'WAP',
+    directories: [
+      {
+        nameDirectory: 'Co so vat chat',
+        listQuery: [
+          {
+            nameQuery: 'So luong giang duong',
+            typeQuery: 1 
+          },{
+            nameQuery: 'Chat luogn giang duong',
+            typeQuery: 1
+          }
+        ]
+      },{
+        nameDirectory: 'Dong gop y kien',
+        listQuery: [
+          {
+            nameQuery: 'Y kien cua ban',
+            typeQuery: 2 
+          }
+        ]
+      }
+    ]
+  };
 
+  show: boolean = true;
+  
   settings: object;
   source: LocalDataSource;
 
-  ngOnInit() {
+  constructor() {}
 
+  @ViewChild('detailModal') detailModal: ModalDirective;
+  @ViewChild('queryModal') queryModal: ModalDirective;
+
+  action: TypeAction;
+
+  ngOnInit() {
+    this.action = TypeAction.View;
   }
 
+  onCreate(){
+    this.action = TypeAction.Create;
+    this.detailModal.show();
+  }
+
+  onCreateQuery(){
+    this.action = TypeAction.Create;
+    this.queryModal.show()
+  }
+
+  onSubmit(){
+
+  }
 
 }
