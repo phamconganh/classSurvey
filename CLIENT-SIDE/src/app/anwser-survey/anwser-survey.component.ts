67446f117d375f0d1c372c6149f2655d55daadfc
HoangPhu98 <hoangvanphu98@gmail.com>
2018-12-28 23:47:50 +0700

    a

--- CLIENT-SIDE/src/app/anwser-survey/anwser-survey.component.ts
+++ CLIENT-SIDE/src/app/anwser-survey/anwser-survey.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-anwser-survey',
@@ -9,7 +9,62 @@ export class AnwserSurveyComponent implements OnInit {
 
   constructor() { }
 
+  data = {
+    isActive: true,
+    createdAt: '10-10-2018',
+    modifiedAt: '11-11-2018',
+    idClassSection:  {
+        semester: '1',
+        idTeacher: 'pnt',
+        time: '',
+        location: '',
+        code: '',
+        name: 'Phat trien ung dung web',
+        creditNumber: 123,
+        idStudents: []
+    },
+    semester: '1',
+    survey: [{
+        section: 'Co so vat chat',
+        dataSection: [{
+            question: 'So luong gian duong dap ung day du'
+        }, {
+          question: 'Chat luong gian duong'
+        }]
+    }, {
+      section: 'Hoat dong giang day',
+        dataSection: [{
+            question: 'Truyen dat day du kien thuc'
+        }, {
+          question: 'Mo rong kien thuc thuc te'
+        }]
+    }]
+  };
+
+  @Input() rating: number;
+  @Input() itemId: number;
+  @Output() ratingClick: EventEmitter<any> = new EventEmitter<any>();
+
+  inputName: string;
+
   ngOnInit() {
+    this.inputName = this.itemId + '_rating';
+  }
+
+  onClick(rating: number): void {
+    this.rating = rating;
+    this.ratingClick.emit({
+      itemId: this.itemId,
+      rating: rating
+    });
+  }
+
+  onClickx(rating: number, queryId: number): void {
+    this.rating = rating;
+    this.ratingClick.emit({
+      itemId: this.itemId,
+      rating: rating
+    });
   }
 
 }
--- CLIENT-SIDE/src/app/manage-survey/manage-survey.component.ts
+++ CLIENT-SIDE/src/app/manage-survey/manage-survey.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { Ng2SmartTableModule, LocalDataSource } from 'ng2-smart-table';
 import { ModalDirective } from 'ngx-bootstrap/modal';
 import { TypeAction } from '../app.entity';
+import { FormBuilder, FormGroup } from '@angular/forms';
+// import { currentId } from 'async_hooks';
 
 @Component({
   selector: 'app-manage-survey',
@@ -13,60 +15,132 @@ import { TypeAction } from '../app.entity';
 
 export class ManageSurveyComponent implements OnInit {
 
-  data: object = {
-    nameSubject: 'WAP',
-    directories: [
-      {
-        nameDirectory: 'Co so vat chat',
-        listQuery: [
-          {
-            nameQuery: 'So luong giang duong',
-            typeQuery: 1 
-          },{
-            nameQuery: 'Chat luogn giang duong',
-            typeQuery: 1
-          }
-        ]
-      },{
-        nameDirectory: 'Dong gop y kien',
-        listQuery: [
-          {
-            nameQuery: 'Y kien cua ban',
-            typeQuery: 2 
-          }
-        ]
-      }
-    ]
+  data = {
+    isActive: true,
+    createdAt: '10-10-2018',
+    modifiedAt: '11-11-2018',
+    idClassSection:  {
+        semester: '1',
+        idTeacher: 'pnt',
+        time: '',
+        location: '',
+        code: '',
+        name: 'Phat trien ung dung web',
+        creditNumber: 123,
+        idStudents: []
+    },
+    semester: '1',
+    survey: [{
+        section: 'Co so vat chat',
+        dataSection: [{
+            question: 'So luong gian duong dap ung day du'
+        }, {
+          question: 'Chat luong gian duong'
+        }]
+    }, {
+      section: 'Hoat dong giang day',
+        dataSection: [{
+            question: 'Truyen dat day du kien thuc'
+        }, {
+          question: 'Mo rong kien thuc thuc te'
+        }]
+    }]
   };
 
-  show: boolean = true;
-  
+  show = true;
   settings: object;
   source: LocalDataSource;
+  formSurvey: FormGroup;
 
-  constructor() {}
+  constructor(private formbuilder: FormBuilder) {}
 
   @ViewChild('detailModal') detailModal: ModalDirective;
   @ViewChild('queryModal') queryModal: ModalDirective;
+  @ViewChild('nameSectionModal') nameSectionModal: ModalDirective;
 
   action: TypeAction;
 
   ngOnInit() {
     this.action = TypeAction.View;
+    this.formSurvey = this.formbuilder.group({
+      test: '',
+      editName: '',
+      query: '',
+    });
   }
 
-  onCreate(){
+  onCreate() {
     this.action = TypeAction.Create;
     this.detailModal.show();
   }
 
-  onCreateQuery(){
+  onCreateQuery() {
     this.action = TypeAction.Create;
-    this.queryModal.show()
+    this.queryModal.show();
   }
 
-  onSubmit(){
+  // current = ...
 
+  editNameSection(data) {
+    // current = data;
+    this.action = TypeAction.Create;
+    this.nameSectionModal.show();
+  }
+
+  onSubmit() {
+    const newSection = this.formSurvey.value.test;
+    this.data.survey.push({
+      section: newSection,
+      dataSection: []
+    });
+    console.log('new Data: ' + this.data);
+    alert('Nhom cau hoi ' + newSection + ' da duoc them thanh cong' );
+    this.detailModal.hide();
+  }
+
+  onSubmitNameSection() {
+    const newNameSection = this.formSurvey.value.editName;
+    this.data.survey.forEach(e => {
+    });
+  }
+
+  onQuerySubmit() {
+    const newQuery = this.formSurvey.value.query;
+
+    console.log('new Data: ' + this.data);
+    alert('Nhom cau hoi ' + newQuery + ' da duoc them thanh cong' );
+    this.queryModal.hide();
   }
 
 }
+
+/*
+isActive: Boolean, da publish hay chua
+createdAt: String,
+modifiedAt: String,
+idClassSection: 'int3306',
+[
+    semester: String,
+    idTeacher: String,
+    time: String,
+    location: String,
+    code: String,
+    name: String,
+    creditNumber: Number,
+    idStudents: [String]
+]
+semester: String,
+data: [{
+    section: String,
+    dataSection: [{
+        question: String
+    }]
+}]
+
+idSurveyForm: {type: Schema.Types.ObjectId, ref: 'SurveyForm'},
+idClassSection: {type: Schema.Types.ObjectId, ref: 'ClassSection'},
+data: [String]
+
+json -> conver sang string
+
+ */
